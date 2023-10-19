b84cbb0495aed0cd984296ae7699e617e13a7459
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-10-19 11:29:07 +0700

    fix side hustle

--- components/elements/about/side_hustle.tsx
+++ components/elements/about/side_hustle.tsx
@@ -1,9 +1,13 @@
 import InfoSection from '@/components/ui/info_section';
+import { RouteConfig } from '@/constants';
 import { PartnerCalling } from '@/ui/img-resource/ImageResources';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const SideHustle = () => {
+  const router = useRouter();
+
   return (
     <div className="bg-neutral-100">
       <InfoSection
@@ -16,17 +20,17 @@ const SideHustle = () => {
           title: 'The Ultimate Side Hustle',
           ctaConfig: {
             title: 'Learn more',
-            onClick: () => {},
+            onClick: () => router.push(RouteConfig.Partner),
           },
           infoClassName: 'font-semibold justify-center md:gap-6',
           desc: 'Partner with ExtraBread today and start your side hustle journey!',
         }}
         image={
-          <div className="flex-1 md:h-[310px]">
+          <div className="flex-1 md:h-[244px]">
             <Image
               src={PartnerCalling}
               alt="partner"
-              className=" md:h-[310px] object-cover rounded-xl"
+              className=" md:h-[244px] object-cover rounded-xl"
             />
           </div>
         }
