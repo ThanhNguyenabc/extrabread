1a0efa0495b2a19d5923571e892c854f3cc38b34
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-12-18 12:11:54 +0700

    translate about_us page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -17,28 +17,31 @@ import { CTAInnerFooter } from '@/ui/organisms/cta-inner-footer/CTAInnerFooter';
 import { UniqueValue } from '@/ui/organisms/unique-value/UniqueValue';
 import { WorkWithTheBest } from '@/ui/organisms/work-with-the-best/WorkWithTheBest';
 import { Seo } from '@/ui/util-components/Seo';
+import HTMLReactParser from 'html-react-parser';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { GetStaticProps } from 'next/types';
 import React from 'react';
 import { getSEOTag } from './api/app-configs';
+
 const DataStatistics = [
   {
     title: '$100K',
-    desc: 'Earn up to $100K with a cash signing bonus',
+    desc: 'earn_with',
   },
   {
     title: '30+',
-    desc: 'Over 30+ years of experience in the field',
+    desc: 'experience',
   },
   {
     title: '500',
-    desc: 'Taking pride with over 500 happy clients',
+    desc: 'happy_clients',
   },
   {
     title: '20+',
-    desc: 'Choose from our wide range of 20+ POS systems',
+    desc: 'pos_systems',
   },
 ];
 
@@ -46,49 +49,49 @@ const InfoSections = [
   {
     image: PosSytem,
     imageDirection: 'right',
-    title: 'Point of Sale Solutions',
+    title: 'sale_systems.title',
     ctaConfig: {
-      title: 'Explore POS equipments',
+      title: 'explore_pos',
       href: RouteConfig.SaleSystems,
     },
-    desc: "We offer expert consultation and a comprehensive review of your business's specifics to pinpoint the most suitable POS systems that align seamlessly with your operational requirements. Our extensive network includes partnerships with 20+ industry-leading POS systems, giving us the versatility to present you with options. Plus, we offer POS solutions starting at a low cost of $0.  Learn more about the partners we work with.",
+    desc: 'sale_systems.desc',
   },
   {
     image: ProcessingFee,
     imageDirection: 'left',
-    title: 'Payment Processing Solutions',
+    title: 'payment_solutions.title',
     ctaConfig: {
-      title: 'Payment Processing',
+      title: 'payment_processing',
       href: RouteConfig.PaymentProcessing,
     },
-    desc: 'We offer tailored payment processing solutions for business types.  Current solutions can be complex and eat into your profits with costly fees - with us, you can reduce your fees to $0 & leave it to us to handle the whole setup. Make it easy for yourself and for your customers in accepting cc payments.  Make the switch today for smooth, affordable payment processing.  Ask about our Cash Discount Program.',
+    desc: 'payment_solutions.desc',
   },
   {
     image: Support,
     imageDirection: 'right',
-    title: 'Seamless Installation and 24/7 Support',
+    title: '24/7_support.title',
     ctaConfig: {
-      title: 'Get Started',
+      title: 'get_started',
       href: RouteConfig.Contacts,
     },
-    desc: "We've got your back, 24/7. When you sign up with us, we handle all the logistics, including rolling up our sleeves if necessary. Additionally, we're at your service for any challenges that may arise. We understand the frustration that a faulty POS or payment terminal can bring to your business and well-being. But worry not, we're here to take care of it from day one. Add ExtraBread to your team to seamlessly manage your merchant service needs.",
+    desc: '24/7_support.,desc',
   },
   {
     image: SigningBonus,
     imageDirection: 'left',
-    title: 'Cash Signing Bonus',
+    title: 'signing_bonus.title',
     ctaConfig: {
-      title: 'Get Pricing',
+      title: 'get_pricing',
       href: RouteConfig.GetPricing,
     },
-    desc: "While our services already come with fantastic incentives and savings, the benefits don't stop there. At Extrabread, we go the extra mile by offering a generous cash signing bonus of up to $100k. No strings attached, no interests, contracts, or hidden fees involved. It's our way of expressing gratitude for welcoming you into our Extrabread family. Take the first step and get started now to discover all that awaits you. Get started now to learn more.",
+    desc: 'signing_bonus.desc',
   },
 ];
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   const [seoTag, translation] = await Promise.all([
     getSEOTag('about-us', locale),
-    serverSideTranslations(locale ?? 'en', ['common', 'home']),
+    serverSideTranslations(locale ?? 'en', ['common', 'about_us']),
   ]);
   return {
     props: {
@@ -101,7 +104,8 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 
 const AboutUS = ({ seoTag }: { seoTag?: Meta }) => {
   const { title, description, keywords, image } = seoTag || {};
-
+  const { t: common } = useTranslation();
+  const { t } = useTranslation('about_us');
   const router = useRouter();
 
   const onGetStart = () => {
@@ -114,15 +118,10 @@ const AboutUS = ({ seoTag }: { seoTag?: Meta }) => {
       <BannerX
         leftCmpClassName="w-full items-center"
         headingClassName="text-center md:max-w-[700px] xl:max-w-[1000px]"
-        heading={
-          <>
-            <span className="text-green-500 ">From Consultation to Installation</span>, We Are Your
-            One Stop Shop For All Things Merchant Services.
-          </>
-        }
-        desc="We consult and set your business up with the best POS and Payment Processing Solutions."
+        heading={<>{HTMLReactParser(t('heading'))}</>}
+        desc={t('desc')}
         button={{
-          title: 'Get Started',
+          title: common('get_started'),
           onBtnClick: onGetStart,
         }}
       />
@@ -143,7 +142,7 @@ const AboutUS = ({ seoTag }: { seoTag?: Meta }) => {
                 className="flex flex-col text-white gap-2 text-center md:gap-4"
               >
                 <h3 className="heading-md md:heading-lg">{item.title}</h3>
-                <p className="text-sm-semibold md:text-base">{item.desc}</p>
+                <p className="text-sm-semibold md:text-base">{t(item.desc)}</p>
               </div>
             ))}
           </div>
@@ -152,19 +151,17 @@ const AboutUS = ({ seoTag }: { seoTag?: Meta }) => {
 
       <div className="bg-neutral-100">
         <Hero className="gap-4 md:flex-row md:gap-8 lg:gap-16">
-          <h3 className="heading-xs text-center md:text-start md:heading-lg ">A bit about us</h3>
+          <h3 className="heading-xs text-center md:text-start md:heading-lg ">
+            {t('a_bit_about_us.title')}
+          </h3>
           <div className="flex flex-col gap-4 md:gap-8 md:w-[65%]">
-            <p className=" text-sm whitespace-pre-wrap md:text-lg">
-              {`Extrabread is your one-stop-shop for comprehensive Point of Sale and credit card processing solutions. Our unique strength lies in our partnerships with top-tier POS and CC processing providers, which enables us to offer tailor-made packages with unmatched flexibility.
-              \nWe're not the 'set it and forget it' type – we're here to provide 24/7 support, ensuring a smooth installation process and addressing any issues you encounter along the way. 
-              \nWith over two decades of industry experience, we've weathered the storms and celebrated the successes alongside countless business owners. Count on us to stand by your side every step of the way. Ready to simplify your payment solutions? Get started with a free consultation today.`}
-            </p>
+            <p className=" text-sm whitespace-pre-wrap md:text-lg">{t('a_bit_about_us.desc')}</p>
             <Button
               className=" bg-neutral-900 hover:bg-neutral-900/90"
               size={'responsive'}
               onClick={onGetStart}
             >
-              Get Started
+              {common('get_started')}
             </Button>
           </div>
         </Hero>
@@ -178,9 +175,11 @@ const AboutUS = ({ seoTag }: { seoTag?: Meta }) => {
               className="pb-0 md:pb-0 xl:pb-0"
               dataConfig={{
                 ...item,
+                title: t(item.title),
+                desc: t(item.desc),
                 infoClassName: 'flex-1 justify-center lg:p-7',
                 ctaConfig: {
-                  title: item.ctaConfig.title,
+                  title: common(item.ctaConfig.title),
                   rightIcon: <IcChevronRight />,
                   ctaClassName: 'flex gap-2',
                   onClick: () => router.push(item.ctaConfig.href),
@@ -205,11 +204,7 @@ const AboutUS = ({ seoTag }: { seoTag?: Meta }) => {
       <UniqueValue />
       <SideHustle />
 
-      <CTAInnerFooter
-        htmlText="Discover the perfect point of sale system for your business today!"
-        bonus={2500}
-        sale={250000}
-      />
+      <CTAInnerFooter htmlText={t('footer.heading')} bonus={2500} sale={250000} />
     </>
   );
 };
