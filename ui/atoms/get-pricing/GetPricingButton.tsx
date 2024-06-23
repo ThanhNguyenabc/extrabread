8d45e3d10c0814de0e3700833b3a9b9c30351168
thanhnguyen <thanhnguyen95abc@gmail.com>
2024-06-23 13:46:33 +0700

    fix questionnaire

--- ui/atoms/get-pricing/GetPricingButton.tsx
+++ ui/atoms/get-pricing/GetPricingButton.tsx
@@ -1,24 +1,23 @@
 import { QuestionnaireProps } from '@/components/elements/questionnaire/Questionnaire.type';
 import { RouteConfig } from '@/constants/routes';
 import { useTranslation } from 'next-i18next';
-import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useRef } from 'react';
 import { BreadButtonProps, Button } from '../button/Button';
-const Questionnaire = dynamic(() => import('@/components/elements/questionnaire/Questionnaire'));
+import Questionnaire from '@/components/elements/questionnaire/Questionnaire'
 export const GetPricingButton = ({
   title,
   ...props
 }: BreadButtonProps & {
   title?: string;
 }) => {
+  const ref = useRef<QuestionnaireProps>(null);
   const { t } = useTranslation();
   const { pathname } = useRouter();
   const txtButton = title || t('get_pricing');
 
   if (pathname == '/hp2') {
-    const ref = useRef<QuestionnaireProps>(null);
     return (
       <>
         <Button type="primary" {...props} onClick={() => ref.current?.showDialog()}>
