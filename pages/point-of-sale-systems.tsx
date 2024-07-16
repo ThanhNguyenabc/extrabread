cbaf9a28af38a1b20dad5749bb936bd4535e4eb8
thanhnguyen <thanhnguyen95abc@gmail.com>
2024-07-16 17:13:46 +0700

    update footer on the page

--- pages/point-of-sale-systems.tsx
+++ pages/point-of-sale-systems.tsx
@@ -60,7 +60,7 @@ const ElevateYourBusiness = ({ seoTag }: { seoTag?: Meta }) => {
         </Hero>
       </div>
       <SideHustle />
-      <CTAInnerFooter htmlText={t('footer.heading')} bonus={2500} sale={250000} />
+      <CTAInnerFooter htmlText={t('footer.heading')}/>
     </>
   );
 };
