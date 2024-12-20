3039787afdbfc8888bd389596a5d79f1b8e80fb2
thanhnguyen <thanhnguyen95abc@gmail.com>
2024-12-20 17:34:18 +0700

    edit request-demo-pos page

--- pages/request-demo-pos.tsx
+++ pages/request-demo-pos.tsx
@@ -2,6 +2,8 @@ import RequestDemoPOS from '@/components/elements/request_demo_pos/RequestDemoPO
 import Flex from '@/components/ui/flex';
 import { RouteConfig } from '@/constants/routes';
 import { Meta } from '@/models/app_config.model';
+import { CTAInnerFooter } from '@/ui/organisms/cta-inner-footer/CTAInnerFooter';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import { GetStaticProps } from 'next/types';
@@ -24,6 +26,7 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 };
 
 const HomePage = ({ seoTag }: { seoTag?: Meta }) => {
+  const { t: common } = useTranslation();
   const { title, description, keywords, image } = seoTag || {};
   const router = useRouter();
 
@@ -32,11 +35,15 @@ const HomePage = ({ seoTag }: { seoTag?: Meta }) => {
       router.replace(RouteConfig.Home);
     }, 1500);
   }, []);
+
   return (
-    <Flex className="w-full lg:max-w-2xl mx-auto">
-      <Seo title={title} description={description} keywords={keywords} imageFeature={image}></Seo>
-      <RequestDemoPOS showCloseButton={false} afterSubmit={navigateToHomePage} />
-    </Flex>
+    <>
+      <Flex className="w-full lg:max-w-2xl mx-auto">
+        <Seo title={title} description={description} keywords={keywords} imageFeature={image}></Seo>
+        <RequestDemoPOS showCloseButton={false} afterSubmit={navigateToHomePage} />
+      </Flex>
+      <CTAInnerFooter htmlText={common('footer.heading')} bonus={2500} sale={250000} />
+    </>
   );
 };
 
